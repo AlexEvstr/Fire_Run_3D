dac5f3a02667ab844affd126141e74d5fdbc431d
Alexander Evstratov <alexdrugev@me.com>
2023-11-15 14:39:54 +0300

    Add score and bestScore.

--- Assets/_Scripts/Buttons.cs
+++ Assets/_Scripts/Buttons.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject _gameOverPanel;
+    [SerializeField] private Text _menuBestText;
+
+
+    private void Start()
+    {
+        int bestScore = PlayerPrefs.GetInt("bestScore", 0);
+        _menuBestText.text = "Best: " + bestScore.ToString();
+        Time.timeScale = 0;
+        int start = PlayerPrefs.GetInt("start", 0);
+        if (start == 1)
+        {
+            _menuPanel.SetActive(false);
+            PlayerPrefs.SetInt("start", 0);
+            Time.timeScale = 1;
+        }
+    }
 
     public void Restart()
     {
         SceneManager.LoadScene("SampleScene");
+        PlayerPrefs.SetInt("start", 1);
+    }
+
+    public void StartGame()
+    {
+        _menuPanel.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("SampleScene");
+        PlayerPrefs.SetInt("start", 0);
     }
 }
--- Assets/_Scripts/Score.cs
+++ Assets/_Scripts/Score.cs
@@ -6,16 +6,28 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Text _gameOverScoreText;
+    [SerializeField] private Text _gameOverBestText;
+    
     private int _score;
+    private int _bestScore;
 
     private void Start()
     {
+        _bestScore = PlayerPrefs.GetInt("bestScore", 0);
         StartCoroutine(IncreaseScore());
     }
 
     private void Update()
     {
+        if (_bestScore < _score)
+        {
+            _bestScore = _score;
+            PlayerPrefs.SetInt("bestScore", _bestScore);
+        }
         _scoreText.text = "Score  " + _score.ToString();
+        _gameOverScoreText.text = "Score  " + _score.ToString();
+        _gameOverBestText.text = "Best: " + _bestScore.ToString();
     }
 
     private IEnumerator IncreaseScore()
